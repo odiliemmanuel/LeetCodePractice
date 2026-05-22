a470a4eb091c70d434a5ad5e8a043e7be3bf325b
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-22 13:46:57 +0100

    "Leetcode practice"

--- src/medium/AddTwoNumbers.java
+++ src/medium/AddTwoNumbers.java
@@ -7,7 +7,7 @@ public class AddTwoNumbers {
     public static void main(String[] args) {
 
         int[] list = {2, 4, 3};
-        int [] list2 = {5, 6, 9};
+        int [] list2 = {5, 6, 4};
 
         ArrayList<Integer> result = addTwoNumbers(list, list2);
         System.out.println(result);
@@ -15,21 +15,21 @@ public class AddTwoNumbers {
 
     public static ArrayList<Integer> addTwoNumbers(int[] num1, int[] num2) {
         ArrayList<Integer> result = new ArrayList<>();
-        int sum = 0;
+
         for(int count = 0; count < num1.length; count ++) {
-            for (int index = 0;  index < num2.length; index ++) {
-               sum = num1[count] + num2[index];
+
+              int sum = num1[count] + num2[count];
 
                 if (sum > 9) {
                     sum = sum - 10;
 
+                    result.add(sum);
+                }
+                else {
+                    result.add(sum);
+
                 }
 
-                num1[count] += 1;
-
-            }
-
-            result.add(sum);
 
         }
 
