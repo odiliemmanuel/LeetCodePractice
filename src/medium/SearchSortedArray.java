bfcf66713a718004941b2cc4afa0e19987a4c9d4
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-23 19:45:04 +0100

    "Leetcode practice"

--- src/medium/SearchSortedArray.java
+++ src/medium/SearchSortedArray.java
@@ -1,23 +1,12 @@
 package medium;
 
+
 public class SearchSortedArray {
     public static void main(String[] args) {
 
-        int[] nums1 = {4,5,6,7,0,1,2};
-        int target = 3;
-        int result = searchNumber(nums1, target);
-        System.out.println(result);
-
     }
 
-    public static int searchNumber(int [] number, int target) {
-        for (int count = 0; count < number.length; count++) {
 
-            if (number[count] == target) {
-                return count;
-            }
 
-        }
-        return -1;
-    }
+    public
 }
